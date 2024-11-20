bbc36477f2375f9f037f8893db98eadd6cfae391
andrea_merlos <merlosandrea13@gmail.com>
2024-11-20 08:49:25 -0500

    added comments

--- contact.js
+++ contact.js
@@ -1,8 +1,9 @@
+// gets button elemnt and attach evenListeener that runs the function
 document.getElementById("button").addEventListener("click", function (event) {
-  // Prevent form submission
+  // Prevent function form submission from reloading
   event.preventDefault();
 
-  // Get form values
+  // Get form value from input then using a method called trim witch makes it easier to validate
   const firstName = document.getElementById("firstName").value.trim();
   const lastName = document.getElementById("lastName").value.trim();
   const email = document.getElementById("email").value.trim();
@@ -12,45 +13,57 @@ document.getElementById("button").addEventListener("click", function (event) {
   // Validation checks
   let isValid = true;
   let errorMessage = "";
-
+  // no input then first name will be false and will run the code the error message and will change the variable validatoin
   if (!firstName) {
     errorMessage += "First Name is required.\n";
     isValid = false;
   }
+  // no input then first name will be false and will run the code the error message and will change the variable validatoin (! si no)
   if (!lastName) {
     errorMessage += "Last Name is required.\n";
     isValid = false;
   }
   if (!email || !validateEmail(email)) {
+    // checks if its not empty or if the email pattern is false
     errorMessage += "A valid Email is required.\n";
+    // no input then email will be false and will run the code the error message and will change the variable validatoin
     isValid = false;
   }
   if (!phone || !validatePhone(phone)) {
+    // checks if its not empty or if the phone pattern is false
     errorMessage += "A valid Phone Number is required.\n";
+    // no input then phone will be false and will run the code the error message and will change the variable validatoin
     isValid = false;
   }
   if (!comments) {
     errorMessage += "Comments are required.\n";
     isValid = false;
+    // no input then comments will be false and will run the code the error message and will change the variable validatoin
   }
 
   // Show error or success
   if (!isValid) {
     alert(errorMessage);
+    // creates an alert with error message
   } else {
     alert("Form submitted successfully!");
+    // if true then will create alert with sucess message
   }
 });
 
 // Helper function to validate email
 function validateEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // creates pattern to check pattern of email
   return regex.test(email);
+  // checks if the value has the pattern that is set will return true or false
 }
 
 // Helper function to validate phone number (example: 10-digit numbers)
 function validatePhone(phone) {
   const regex = /^[0-9]{10}$/;
+  // creates pattern to check pattern of phone
   return regex.test(phone);
+  // checks if the value has the pattern that is set will return true or false
 }
 // Thank you for checking my work
--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   var elems = document.querySelectorAll(".sidenav");
-//   var instances = M.Sidenav.init(elems, options);
-// });
-
 fetch(`heels.json`)
+  // method of fetch from json file
   .then((response) => response.json())
+  // after feching data it creates an arrow function that transforms the response (object)
   .then((data) => {
+    // after the data is formated as an object
     data.heels.forEach((element) => {
+      //get data from heels and for each element run the next code
       console.log(element);
       let picture = element["img"];
+      // variable created to save the dif values from data
       let name = element["name"];
+      // variable created to save the dif values from data
       let discription = element["shoe-description"];
+      // variable created to save the dif values from data
       let price = element["shoe price"];
+      // variable created to save the dif values from data
 
       const heelsCard = document.createElement("div");
+      // creating variable and the creating an HTML div
 
-      heelsCard.classList.add["heelsView"];
+      heelsCard.classList.add("heelsView");
+      // div created is accessing to calss list and adding the new class called heelsView
+
+      //  creating another variable
       const html = `
       <div class="heelImages"><img src="${picture}" class="pictures"</div>
       <p class="shoe-name">${name}</p>
@@ -24,6 +31,8 @@ fetch(`heels.json`)
       <div class="addCart"><button class="addToCart">Buy Now</button></div>`;
 
       heelsCard.innerHTML = html;
+      // getting div created heelsCard and adding the variable html that i created with my info
       document.getElementById("heels-container").appendChild(heelsCard);
+      // getting my element by id heels-container from html and adding as a child the div element created with all my data
     });
   });
